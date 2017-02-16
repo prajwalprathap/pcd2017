529d489e23e28bd715eb7b4d484ebfba88b58762
Your Name <you@example.com>
2017-02-16 10:58:04 +0530

    .

---
+++ week1/prajwal.c
@@ -0,0 +1,12 @@
+
+
+#include<stdio.h>
+int  main()
+{int a, b,sum;
+printf("\nEnter two  no:");
+scanf("%d %d",&a, &b);
+sum=a+b;
+printf("sum : %d",sum);
+return(0);}
+
+
---
+++ week1/prajwal1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+ {int a, b,c,sum;
+ printf("\nEnter three no:");
+ scanf("%d%d%d",&a, &b,&c);
+ sum=a+b+c;
+printf("sum : %d",sum);
+                      return(0);}
